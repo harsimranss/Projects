b97caf442fe0a40a10b02f63455b1e922211ee9c
harsimran-singh-99 <harsimran0199@gmail.com>
2021-03-23 09:14:48 +0530

    random walk added

---
+++ random walk.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a,b,c=plt.hist(np.random.binomial(1000,0.9,100000),bins=range(900,1001))
+plt.close()
+plt.figure(2)
+plt.plot(range(900,1000),a)
+plt.show()
+
+
